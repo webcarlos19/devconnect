69738630e39f34d6aeb48e1430040a6de3d71914
Jackson <jacksonoliveirasssss@gmail.com>
2025-10-31 20:42:57 -0300

    feat: create navbar (#2)
    Co-authored-by: Carlos Eduardo <83090102+webcarlos19@users.noreply.github.com>

--- frontend/src/pages/app/Feed.jsx
+++ frontend/src/pages/app/Feed.jsx
@@ -1,6 +1,11 @@
 function Feed() {
     return (
         <div className="container mx-auto px-4 py-8">
+
+            {/* 🔵 Bolhas de fundo */}
+        <div className="absolute top-0 left-0 w-50 h-60 bg-[#28e1ed] opacity-40 rounded-full blur-3xl transform-translate-x1/2 -translate-y-1/2"></div>
+        <div className="absolute bottom-0 right-1 w-64 h-64 bg-[#28e1ed] opacity-20 rounded-full blur-3xl translate-y-1/2"></div>
+        
             <h1 className="text-4xl font-bold text-blue-700 mb-8">SOCIAL.DEV - Feed</h1>
 
             <div className="grid gap-4">
