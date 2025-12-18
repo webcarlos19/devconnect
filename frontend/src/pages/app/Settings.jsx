d1fed7a77088a54e7cb509d3dc1b8ca134221b07
Jenniferwin74 <jenniferprogamadora@gmail.com>
2025-12-18 16:45:16 -0300

    Created the settings page (#17)

--- frontend/src/pages/app/Settings.jsx
+++ frontend/src/pages/app/Settings.jsx
@@ -1,13 +1,16 @@
+import UserSettings from '../../components/App/Settings/UserSettings';
+
 function Settings() {
     return (
         <div className="container mx-auto px-4 py-8">
-            <h1 className="text-4xl font-bold text-blue-700 mb-8">SOCIAL.DEV - Configurações</h1>
-
+        
             <div className="grid gap-4">
-                <h2 className="text-2xl font-semibold mb-4">Configurações do Usuário</h2>
+                
             </div>
+
+            <UserSettings />
         </div>
     )
 }
 
-export default Settings
+export default Settings;
